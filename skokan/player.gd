d35973ec194650c5b22920ea391f48d96218e740
Kango <159473005+SamuelKovac@users.noreply.github.com>
2024-12-29 12:39:31 +0100

    pridanie postavičky, jej pohyb, animácie

--- skokan/player.gd
+++ skokan/player.gd
@@ -1,26 +1,41 @@
 extends CharacterBody2D
 
-# Movement parameters
-@export var speed = 150           # Horizontal movement speed
-@export var jump_force = -300    # Jumping impulse
-@export var gravity = 800         # Gravity applied to the character
+@export var speed = 150          
+@export var jump_force = -300    
+@export var gravity = 800         
 
 func _physics_process(delta):
-	# Apply gravity
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Horizontal movement
-	if Input.is_action_pressed("ui_left"):
+	# Horizontálny pohyb (na podlahe aj vo vzduchu)
+	if Input.is_action_pressed("left"):
 		velocity.x = -speed
-	elif Input.is_action_pressed("ui_right"):
+		$AnimatedSprite2D.flip_h = true
+		if is_on_floor():
+			$AnimatedSprite2D.play("run")
+	elif Input.is_action_pressed("right"):
 		velocity.x = speed
+		$AnimatedSprite2D.flip_h = false
+		if is_on_floor():
+			$AnimatedSprite2D.play("run")
 	else:
-		velocity.x = 0
+		if is_on_floor():
+			velocity.x = 0
+			$AnimatedSprite2D.play("idle")
 
-	# Jumping
-	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
+	
+	if not is_on_floor():
+		if velocity.y < 0:  # Skok
+			$AnimatedSprite2D.play("jump")
+		elif velocity.y > 0:  # Pád
+			$AnimatedSprite2D.play("fall")
+
+	# Skok
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = jump_force
+		$AnimatedSprite2D.play("jump")
 
-	# Move the character
+	# Aplikácia pohybu
 	move_and_slide()
