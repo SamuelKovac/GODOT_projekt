b0d2a0bbd1b3595fa0b83ef7a576893bba403190
Kolinek <113975842+Kolinek@users.noreply.github.com>
2024-12-30 17:10:28 +0100

    coin sound

--- skokan/scripts/gcoin.gd
+++ skokan/scripts/gcoin.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
+@onready var gcoin_sound: AudioStreamPlayer2D = $gcoin_sound
+
+var is_collected: bool = false
+
 func _on_body_entered(body: Node2D) -> void:
-		if body.is_in_group("Player"):
-			GameManager.coins += 1
-			GameManager.score += 100
-			queue_free()
-			
+	if body.is_in_group("Player") and not is_collected:
+		is_collected = true
+		GameManager.coins += 1
+		GameManager.score += 100
+		gcoin_sound.play()
+		hide()
