95ea7e33f6c927840094d0e45208e4fd3aeb88e9
Kango <159473005+SamuelKovac@users.noreply.github.com>
2024-12-30 13:29:23 +0100

    Pridanie levelov
    Level1 - úprava
    Level2 - generovanie (treba tematický parallax)
    Level3 - generovanie (treba tematický parallax)
    Level4 - generovanie (treba tematický parallax)

--- skokan/scripts/next_lvl.gd
+++ skokan/scripts/next_lvl.gd
@@ -1,13 +1,19 @@
 extends Area2D
-
+var required_coins = 10
 func _on_body_entered(body: Node2D) -> void:
+	
 	if body.is_in_group("Player"):
-		var current_scene_file = get_tree().current_scene.scene_file_path
-		var next_level_number = current_scene_file.to_int() + 1
+		
+		if GameManager.coins >= required_coins:
+			GameManager.coins = 0 
+			var current_scene_file = get_tree().current_scene.scene_file_path
+			var next_level_number = current_scene_file.to_int() + 1
 
-		var next_lvl_path = "res://lvl_" + str(next_level_number) + ".tscn"
+			var next_lvl_path = "res://lvl_" + str(next_level_number) + ".tscn"
 
-		call_deferred("_change_scene", next_lvl_path)
+			call_deferred("_change_scene", next_lvl_path)
 
 func _change_scene(next_lvl_path: String) -> void:
 	get_tree().change_scene_to_file(next_lvl_path)
+
+	  
