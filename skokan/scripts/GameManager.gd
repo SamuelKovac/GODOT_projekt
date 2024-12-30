ccce30325b58eff021a29cff3704cb2dd958b77d
Kolinek <113975842+Kolinek@users.noreply.github.com>
2024-12-30 16:24:50 +0100

    coiny
    done

--- skokan/scripts/GameManager.gd
+++ skokan/scripts/GameManager.gd
@@ -4,5 +4,11 @@ var coins = 0
 var score = 0
 
 func _process(_delta: float) -> void:
-	$"GUI/coins".text = str(coins)
+	$"GUI/coins".text = str(coins) + "/10"
 	$"GUI/score".text = str(score)
+
+	var coin_label = $"GUI/coins"
+	if coins < 10:
+		coin_label.modulate = Color(1, 0, 0)
+	else:
+		coin_label.modulate = Color(0, 1, 0)
